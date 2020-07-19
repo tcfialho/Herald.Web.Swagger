c2151f10deb3d66d014a84349e3165ac1921ee59
Thiago F <tcfialho@gmail.com>
2020-07-19 18:45:18 -0300

    fix: duplicated code in startup webapistub (#4)

--- Herald.Web.Swagger.Tests.WebApiStub/Startup.cs
+++ Herald.Web.Swagger.Tests.WebApiStub/Startup.cs
@@ -16,22 +16,18 @@ namespace Herald.Web.Swagger.Tests.WebApiStub
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwagger();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseSwagger();
 
             app.UseHttpsRedirection();
 
