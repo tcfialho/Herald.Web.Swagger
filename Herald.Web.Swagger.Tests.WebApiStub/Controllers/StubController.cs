b5e73bd49c32f0aefeb2e7a208be611fe4ddac84
Thiago F <tcfialho@gmail.com>
2020-07-19 04:29:25 -0300

    feat: swagger configurations (#2)
    * feat: swagger configurations

---
+++ Herald.Web.Swagger.Tests.WebApiStub/Controllers/StubController.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Herald.Web.Swagger.Tests.WebApiStub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StubController : ControllerBase
+    {
+        public StubController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Herald.Web.Swagger.Tests/SwaggerTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Herald.Web.Swagger.Tests
+{
+    public class SwaggerTests : IClassFixture<WebApplicationFactory<WebApiStub.Startup>>
+    {
+        private readonly WebApplicationFactory<WebApiStub.Startup> _factory;
+
+        public SwaggerTests(WebApplicationFactory<WebApiStub.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ShouldSwaggerUIWhenAddSwaggerIsOK()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/swagger");
+
+            // Assert            
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ShouldGetSwaggerJsonWhenUseSwaggerIsOK()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/swagger/v1.0.0.0/swagger.json");
+
+            // Assert            
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
