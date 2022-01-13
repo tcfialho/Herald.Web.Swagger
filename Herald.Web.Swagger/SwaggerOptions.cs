2c6214111cc30b9633352e6cd6781f27c7c7d5c1
Thiago F <tcfialho@gmail.com>
2022-01-13 20:45:35 -0300

    feat: operation filter support (#44)
    * feat: operation filter support

--- Herald.Web.Swagger/SwaggerOptions.cs
+++ Herald.Web.Swagger/SwaggerOptions.cs
@@ -1,9 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+
+using System.Collections.Generic;
 
 namespace Herald.Web.Swagger
 {
     public class SwaggerOptions
     {
         public IEnumerable<string> Servers { get; set; }
+
+        internal List<FilterDescriptor> OperationFilterDescriptors { get; set; } 
+
+        public SwaggerOptions()
+        {
+            OperationFilterDescriptors = new List<FilterDescriptor>();
+        }
+
+        public void OperationFilter<TFilter>(params object[] arguments) where TFilter : IOperationFilter
+        {
+            OperationFilterDescriptors.Add(new FilterDescriptor
+            {
+                Type = typeof(TFilter),
+                Arguments = arguments
+            });
+        }
     }
 }
